01a85d18d0358bb4beef4e7f710b675eda64ee1f
rajarushil <rajarushil.reddy@gmail.com>
2019-05-24 16:36:23 -0500

    5/24/2019

---
+++ Javascript/05-22-2019/calculator.js
@@ -0,0 +1,12 @@
+const calculator = {
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
+function updateDisplay() {
+  const display = document.querySelector('.screen');
+  display.value = calculator.displayValue;
+}
+
+updateDisplay();
