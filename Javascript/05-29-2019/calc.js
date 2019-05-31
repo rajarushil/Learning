dc43b671fbf2126c43fc8e4d7604d34349b1f4e5
rajarushil <rajarushil.reddy@gmail.com>
2019-05-31 13:13:52 -0500

    calc final

---
+++ Javascript/05-29-2019/calc.js
@@ -0,0 +1,27 @@
+function calc()
+            {
+                var t1 = parseFloat(document.getElementById('t1').value);
+                var t2 = parseFloat(document.getElementById('t2').value);
+                
+                var oper = document.getElementById('operators').value;
+                
+                if(oper === '+')
+                {
+                    document.getElementById('result').value = t1+t2;
+                }
+                
+                if(oper === '-')
+                {
+                    document.getElementById('result').value = t1-t2;
+                }
+                
+                if(oper === '/')
+                {
+                    document.getElementById('result').value = t1/t2;
+                }
+                
+                if(oper === '*')
+                {
+                    document.getElementById('result').value = t1*t2;
+                }
+            }
