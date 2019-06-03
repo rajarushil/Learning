1557104baa064b8886645699d33ba85d86adbde5
rajarushil <rajarushil.reddy@gmail.com>
2019-06-03 00:36:53 -0500

    trial

--- Javascript/05-29-2019/calc.js
+++ Javascript/05-29-2019/calc.js
@@ -1,28 +1,25 @@
 function calc()
-            {
-                var t1 = parseFloat(document.getElementById('t1').value);
-                var t2 = parseFloat(document.getElementById('t2').value);
+ {
+var t1 = parseFloat(document.getElementById('t1').value);
+var t2 = parseFloat(document.getElementById('t2').value);
                 
-                var oper = document.getElementById('operators').value;
-                
-                if(oper === '+')
+var oper = document.getElementById('operators').value;
+if(oper === '+')
                 {
-                    document.getElementById('result').value = t1+t2;
+  document.getElementById('result').value = t1+t2;
                 }
                 
-                if(oper === '-')
+ if(oper === '-')
                 {
-                    document.getElementById('result').value = t1-t2;
+ document.getElementById('result').value = t1-t2;
                 }
                 
-                if(oper === '/')
+  if(oper === '/')
                 {
-                    document.getElementById('result').value = t1/t2;
+ document.getElementById('result').value = t1/t2;
                 }
-                
-                if(oper === '*')
-                {
-                    document.getElementById('result').value = t1*t2;
+      if(oper === '*')
+    {
+ document.getElementById('result').value = t1*t2;
                 }
             }
-result;
---
+++ Javascript/06-01-2019/table.js
@@ -0,0 +1,12 @@
+function table()
+{
+    var num = document.getElementById(num1).value;
+    document.getElementById(num2).value = 2*1;
+    document.getElementById(num3).value 
+    document.getElementById(num4).value
+    document.getElementById(num5).value
+    document.getElementById(num6).value
+    document.getElementById(num7).value
+    document.getElementById(num8).value
+    document.
+}
