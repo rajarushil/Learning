90979f1a39e4e631a90ab7c453caa9b592e28c28
rajarushil <rajarushil.reddy@gmail.com>
2019-06-05 22:53:00 -0500

    table 1.1

---
+++ Javascript/06-05-2019/multiply.js
@@ -0,0 +1,37 @@
+      function multiply () {
+          var numOne = document.getElementById('num1').value;
+          var numTwo = document.getElementById('num2').value;
+           var numThree = document.getElementById('num3').value;
+           var numFour = document.getElementById('num4').value;
+           var numFive = document.getElementById('num5').value;
+           var numSix = document.getElementById('num6').value;
+           var numSeven = document.getElementById('num7').value;
+           var numEight = document.getElementById('num8').value;
+           var numNine = document.getElementById('num9').value;
+           var numTen = document.getElementById('num10').value;
+          
+    var numA= document.getElementById('n1').value;
+          var numB = document.getElementById('n2').value;
+           var numC = document.getElementById('n3').value;
+           var numD = document.getElementById('n4').value;
+           var numE = document.getElementById('n5').value;
+           var numF = document.getElementById('n6').value;
+           var numG = document.getElementById('n7').value;
+           var numH = document.getElementById('n8').value;
+           var numI = document.getElementById('n9').value;
+           var numJ = document.getElementById('n10').value;
+          
+        var result= {
+        document.getElementById('r1').value = num1*n1;
+        document.getElementById('r2').value = num2*n2;
+        document.getElementById('r3').value = num3*n3;
+        document.getElementById('r4').value = num4*n4;
+        document.getElementById('r5').value = num5*n5;
+        document.getElementById('r6').value = num6*n6;
+        document.getElementById('r7').value = num7*n7;
+        document.getElementById('r8').value = num8*n8;
+        document.getElementById('r9').value = num9*n9;
+        document.getElementById('r10').value = num10*n10;
+        }
+
+}
