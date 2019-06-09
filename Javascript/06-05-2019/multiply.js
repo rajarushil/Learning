0180005c25516dd16afd9c3d81a1110a6605b971
rajarushil <rajarushil.reddy@gmail.com>
2019-06-08 23:32:39 -0500

    table 2.1

--- Javascript/06-05-2019/multiply.js
+++ Javascript/06-05-2019/multiply.js
@@ -1,14 +1,5 @@
       function multiply () {
           var num1 = parseFloat(document.getElementById('num1').value);
-          var num2 = parseFloat(document.getElementById('num2').value);
-           var num3 = parseFloat(document.getElementById('num3').value);
-           var num4 = parseFloat(document.getElementById('num4').value);
-           var num5 = parseFloat(document.getElementById('num5').value);
-           var num6 = parseFloat(document.getElementById('num6').value);
-           var num7 = parseFloat(document.getElementById('num7').value);
-           var num8 = parseFloat(document.getElementById('num8').value);
-           var num9 = parseFloat(document.getElementById('num9').value);
-           var num10 = parseFloat(document.getElementById('num10').value);
           
     var n1=  parseFloat(document.getElementById('n1').value);
           var n2 =  parseFloat(document.getElementById('n2').value);
@@ -23,14 +14,14 @@
           
 
         document.getElementById('r1').value = num1*n1;  
-        document.getElementById('r2').value = num2*n2;
-        document.getElementById('r3').value = num3*n3;
-        document.getElementById('r4').value = num4*n4;
-        document.getElementById('r5').value = num5*n5;
-        document.getElementById('r6').value = num6*n6;
-        document.getElementById('r7').value = num7*n7;
-        document.getElementById('r8').value = num8*n8;
-        document.getElementById('r9').value = num9*n9;
-        document.getElementById('r10').value = num10*n10; 
+        document.getElementById('r2').value = num1*n2;
+        document.getElementById('r3').value = num1*n3;
+        document.getElementById('r4').value = num1*n4;
+        document.getElementById('r5').value = num1*n5;
+        document.getElementById('r6').value = num1*n6;
+        document.getElementById('r7').value = num1*n7;
+        document.getElementById('r8').value = num1*n8;
+        document.getElementById('r9').value = num1*n9;
+        document.getElementById('r10').value = num1*n10; 
     
 }
