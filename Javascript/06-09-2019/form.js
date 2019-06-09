ec660d3d9a16a6e36bfa9e5d11fde4c015947624
rajarushil <rajarushil.reddy@gmail.com>
2019-06-09 14:12:34 -0500

    sign up form

---
+++ Javascript/06-09-2019/form.js
@@ -0,0 +1,10 @@
+function signUp(){
+    var userName = document.getElementById('username');
+    var email = document.getElementById('email');
+    var password= (document.getElementById('password') document.getElementById('re-password');)
+    
+    
+    
+    
+    
+}
